6b1f1937095eaca3b69fa1a887c1a9b5f571830b
yao12310 <brian.yao@berkeley.edu>
2021-05-21 21:17:48 -0700

    pairwise player win % stats

--- utils/stats.py
+++ utils/stats.py
@@ -334,7 +334,7 @@ def player_pair_pcts(thresh=SAMPLE_THRESH, rounding=2, ex_ch=False, df=None):
     """
     Compute percentage of times two players have been on the same team.
     thresh : int
-        minimum number of games both players were on the same team
+        minimum number of games both players were in the same game
     rounding : int
         number of places to round to
     ex_ch : bool
@@ -381,6 +381,126 @@ def player_pair_pcts(thresh=SAMPLE_THRESH, rounding=2, ex_ch=False, df=None):
                 df[-1].append(-1)
             elif player_pair_game_cnts[p1][p2] >= thresh:
                 df[-1].append(player_pair_team_cnts[p1][p2] / player_pair_game_cnts[p1][p2])
+            else:
+                df[-1].append(-1)
+    
+    df = pd.DataFrame(df, columns=players, index=players)
+    df = df.apply(lambda x: np.round(x, rounding))
+    df.index = df.index.rename("Player")
+    
+    return df.reset_index()
+
+def player_pair_win_pcts(thresh=SAMPLE_THRESH, rounding=2, ex_ch=False, df=None):
+    """
+    Compute percentage of times two players have won after being on the same team.
+    thresh : int
+        minimum number of games both players were on the same team
+    rounding : int
+        number of places to round to
+    ex_ch : bool
+        exclude cheesy wins
+    df : pd.DataFrame
+        game data log (if None, fetch from API)
+    return : pd.DataFrame
+    """
+    if df is None:
+        df = fetch_game_log(parse_cols=True)
+    if ex_ch:
+        df = df[~(df[CHEESY_WIN] == 'Yes')]
+    
+    player_cnts = defaultdict(int)
+    player_pair_team_cnts = defaultdict(lambda: defaultdict(int))
+    player_pair_win_cnts = defaultdict(lambda: defaultdict(int))
+    for idx, row in df.iterrows():
+        for role1 in ROLES:
+            player1 = row[role1]
+            player_cnts[player1] += 1
+            for role2 in ROLES:
+                if role1 == role2:
+                    continue
+                player2 = row[role2]
+                if player1 in [UNK, NA] or player2 in [UNK, NA]:
+                    continue
+
+                if role1 in BADS and role2 in BADS:
+                    player_pair_team_cnts[player1][player2] += 1
+                    if row[WINNER] == BAD:
+                        player_pair_win_cnts[player1][player2] += 1
+                if role1 not in BADS and role2 not in BADS:
+                    player_pair_team_cnts[player1][player2] += 1
+                    if row[WINNER] == GOOD:
+                        player_pair_win_cnts[player1][player2] += 1
+                
+    df = []
+    players = list(player_pair_team_cnts.keys())
+    players = list(filter(lambda p: player_cnts[p] >= thresh, players))
+    for p1 in players:
+        df.append([])
+        for p2 in players:
+            if p1 == p2:
+                df[-1].append(-1)
+            elif player_pair_team_cnts[p1][p2] >= thresh:
+                df[-1].append(player_pair_win_cnts[p1][p2] / player_pair_team_cnts[p1][p2])
+            else:
+                df[-1].append(-1)
+    
+    df = pd.DataFrame(df, columns=players, index=players)
+    df = df.apply(lambda x: np.round(x, rounding))
+    df.index = df.index.rename("Player")
+    
+    return df.reset_index()
+
+def player_pair_vs_win_pcts(thresh=SAMPLE_THRESH, rounding=2, ex_ch=False, df=None):
+    """
+    Compute win percentage between all pairs of players when on different teams.
+    thresh : int
+        minimum number of games both players were on different teams
+    rounding : int
+        number of places to round to
+    ex_ch : bool
+        exclude cheesy wins
+    df : pd.DataFrame
+        game data log (if None, fetch from API)
+    return : pd.DataFrame
+    """
+    if df is None:
+        df = fetch_game_log(parse_cols=True)
+    if ex_ch:
+        df = df[~(df[CHEESY_WIN] == 'Yes')]
+    
+    player_cnts = defaultdict(int)
+    player_pair_opp_cnts = defaultdict(lambda: defaultdict(int))
+    player_pair_win_cnts = defaultdict(lambda: defaultdict(int))
+    for idx, row in df.iterrows():
+        for role1 in ROLES:
+            player1 = row[role1]
+            player_cnts[player1] += 1
+            for role2 in ROLES:
+                if role1 == role2:
+                    continue
+                player2 = row[role2]
+                if player1 in [UNK, NA] or player2 in [UNK, NA]:
+                    continue
+
+                if role1 in BADS and role2 not in BADS:
+                    player_pair_opp_cnts[player1][player2] += 1
+                    if row[WINNER] == BAD:
+                        player_pair_win_cnts[player1][player2] += 1
+                if role1 not in BADS and role2 in BADS:
+                    player_pair_opp_cnts[player1][player2] += 1
+                    if row[WINNER] == GOOD:
+                        player_pair_win_cnts[player1][player2] += 1
+                
+    df = []
+    players = list(player_pair_opp_cnts.keys())
+    players = list(filter(lambda p: player_cnts[p] >= thresh, players))
+    for p1 in players:
+        df.append([])
+        for p2 in players:
+            if p1 == p2:
+                df[-1].append(-1)
+            elif player_pair_opp_cnts[p1][p2] >= thresh:
+                df[-1].append(player_pair_win_cnts[p1][p2] / player_pair_opp_cnts[p1][p2])
             else:
                 df[-1].append(-1)
     
